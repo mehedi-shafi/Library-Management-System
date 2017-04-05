88b8fe6998c36a962c34574a9b49faa723268009
Mehedi Shafi <mehedishafi@hotmail.com>
2017-04-06 00:44:19 +0600

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-      
-        function redirect_to($new_location){
-            header("Location: " . $new_location);
-            exit;
-        }
-        redirect_to("production/landing.php");
-?>
